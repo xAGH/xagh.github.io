cbe1c416695cecabc341eb2a8d856a62c00388f2
Alejo <alejito23001@gmail.com>
2023-03-30 02:07:03 -0500

    style: Font family and background image changed.
    The font family is FiraCode, a monospace font provided by Google Fonts
    (check https://fonts.google.com/specimen/Fira+Code).
    
    The background image cover all the screen, that is a personal image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    backgroundImage: {
+      'astronaut': "linear-gradient(rgba(0 0 0 / 0.5), rgba(0 0 0 / 0.5)), url(src/assets/background.jpg)"
+    },
     extend: {},
   },
   plugins: [],
