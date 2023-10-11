162b55727100fefadb2f89a86546b7f7bdfba708
Alejo <alejito23001@gmail.com>
2023-10-11 07:05:28 -0500

    Input changes

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -6,7 +6,7 @@ import { CaretShapes } from 'src/app/core/constants/caret-shapes.const';
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.scss']
 })
 export class InputComponent implements AfterViewInit, OnDestroy{
 
@@ -14,10 +14,10 @@ export class InputComponent implements AfterViewInit, OnDestroy{
   inputTextField: FormControl;
 
   // Reference to the inputTextField length
-  caretGapLength: number;
+  private _caretGapLength: number;
 
   // Suscription reference to inputTextField changes
-  private onChangeSuscription$!: Subscription;
+  private _onChangeSuscription$!: Subscription;
 
   // Element reference to div element that works as care
   @ViewChild('caret') caretDivRef!: ElementRef<HTMLDivElement>
@@ -36,25 +36,60 @@ export class InputComponent implements AfterViewInit, OnDestroy{
     return this._caret;
   };
 
+  before: number = 0;
+
   constructor() {
     // Default values
     this._caret = CaretShapes.BAR;
     this.inputTextField = new FormControl('');
-    this.caretGapLength = this.inputTextField.value.length;
+    this._caretGapLength = this.inputTextField.value.length;
   }
 
   ngAfterViewInit(): void {
-    this.onChangeSuscription$ = this.inputTextField.valueChanges.subscribe(
+    this._onChangeSuscription$ = this.inputTextField.valueChanges.subscribe(
       {
         next: (value: string) => {
-          this.caretDivRef.nativeElement.style.setProperty('--input-length', `${value.length}ch`)
-        },
+          console.log("Value,", value.length);
+          console.log("Shape,", this.len());
+          console.log("Before", this.before);
+
+            this.setCaretGap(value.length)
+          }
+
       }
-    )
+      )
+    }
+
+  private setCaretGap(value: number, add: boolean = false): void {
+    if (add){
+      this.caretDivRef.nativeElement.style.setProperty('--input-length', `${this.len() + value}ch`)
+    }
+    else {
+      this.caretDivRef.nativeElement.style.setProperty('--input-length', `${value}ch`)
+
+    }
+  }
+
+  private len(): number {
+    return parseInt(this.caretDivRef.nativeElement.style.getPropertyValue('--input-length').replace('ch', ''));;
+  }
+
+  logg(e: string) {
+    const len = this.len();
+    if (e == 'left') {
+      if(len > 0) {
+        this.setCaretGap(-1, true)
+      }
+    }
+    else {
+      if(len < this.inputTextField.value.length) {
+        this.setCaretGap(1, true)
+      }
+    }
   }
 
   ngOnDestroy(): void {
-    this.onChangeSuscription$.unsubscribe();
+    this._onChangeSuscription$.unsubscribe();
   }
 
   // Returns the class to be added to the input to customize the caret shape.
